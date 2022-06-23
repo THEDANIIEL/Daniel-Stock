f71f59d98d799d2002dcb3833fef16a6e8d68263
Daniel <dkhozaie@gmail.com>
2022-06-23 09:16:32 +0430

    Initial Commit

---
+++ Daniel Stock/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daniel Stock
+//
+//  Created by Macbook on 4/2/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Daniel Stock/Daniel_StockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Daniel_StockApp.swift
+//  Daniel Stock
+//
+//  Created by Macbook on 4/2/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct Daniel_StockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
